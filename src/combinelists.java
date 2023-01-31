af397aad5fc19476fa50e4df42444c59d1886686
ybao <ybao@baodeMacBook-Pro.local>
2023-01-31 17:54:54 -0500

    1

--- src/combinelists.java
+++ src/combinelists.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 
 public class combinelists {
 	public static void main(String[] args) {
-		ArrayList<Integer> l1 = new ArrayList<Integer>();
-		l1.add(1);
-		l1.add(2);
-		l1.add(3);
-		l1.add(1);
-		l1.add(2);
+		ArrayList<String> l1 = new ArrayList<String>();
+		l1.add("1");
+		l1.add("2");
+		l1.add("3");
+		l1.add("1");
+		l1.add("2");
 		ArrayList<String> l2 = new ArrayList<String>();
 		l2.add("a");
 		l2.add("b");
@@ -30,7 +30,7 @@ public class combinelists {
 	 * @param: one list of integer,one list of string list
 	 * @return: combined list contains all of things in given lists.
 	 */
-	private static ArrayList<String> combine(ArrayList<Integer> l1, ArrayList<String> l2) {
+	private static ArrayList<String> combine(ArrayList<String> l1, ArrayList<String> l2) {
 		int times = 0;
 		int i;
 		int judge;
@@ -45,13 +45,13 @@ public class combinelists {
 		}
 		for(i =0;i<times;i++) {
 			//combine the equal size first
-			l3.add(Integer.toString(l1.get(i)));
+			l3.add(l1.get(i));
 			l3.add(l2.get(i));
 		}
 		//in the case if they do not have equal size;
 		if (judge ==2){
 			for(int j =i;j<l1.size();j++) {
-				l3.add(Integer.toString(l1.get(j)));
+				l3.add(l1.get(j));
 			}
 		}
 		if (judge ==1){
